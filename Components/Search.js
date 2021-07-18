bda31e0a0964f25707304786500195db953dec85
rodriguezny <rodrigueznyiringabo@gmail.com>
2021-07-18 15:16:43 +0200

    ft - Search componet style externalisation

--- Components/Search.js
+++ Components/Search.js
@@ -5,10 +5,21 @@ class Search extends React.Component {
     render() {
         return (
             <View style={{ marginTop: 20 }}>
-                <TextInput style={{ marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5}} placeholder='Titre du film' />
+                <TextInput style={ styles.textInput } placeholder='Titre du film' />
                 <Button title='Rechercher' onPress={() => {}} />
             </View>
         )
+    }
+}
+
+const styles = {
+    textInput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
     }
 }
 
