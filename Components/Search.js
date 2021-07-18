d2915cedeed5ef940c30fe70bdca51d007154aad
rodriguezny <rodrigueznyiringabo@gmail.com>
2021-07-18 14:31:16 +0200

    ft - style application on Search component

--- Components/Search.js
+++ Components/Search.js
@@ -4,8 +4,8 @@ import { View, TextInput, Button } from 'react-native'
 class Search extends React.Component {
     render() {
         return (
-            <View>
-                <TextInput placeholder='Titre du film' />
+            <View style={{ marginTop: 20 }}>
+                <TextInput style={{ marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5}} placeholder='Titre du film' />
                 <Button title='Rechercher' onPress={() => {}} />
             </View>
         )
