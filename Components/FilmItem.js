e287ed906de72111b7aeee3c861769d75ff62696
rodriguezny <rodrigueznyiringabo@gmail.com>
2021-08-03 11:08:02 +0200

    ft - dispalay favorite image in film item component fx implemetation

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -3,6 +3,17 @@ import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 import { getImageFromApi } from '../API/TMDBApi'
 
 class FilmItem extends React.Component {
+    _displayFavoriteImage() {
+        if (this.props.isFilmFavorite) {
+          // Si la props isFilmFavorite vaut true, on affiche le 🖤
+          return (
+            <Image
+              style={styles.favorite_image}
+              source={require('../Images/ic_favorite.png')}
+            />
+          )
+        }
+    }
     render() {
         const { film, displayDetailForFilm } = this.props
         return (
@@ -10,8 +21,9 @@ class FilmItem extends React.Component {
                 <Image style={styles.image} source={{uri: getImageFromApi(film.poster_path)}}/>
                 <View style={styles.contentContainer}>
                     <View style={styles.headerContainer}>
-                    <Text style={styles.titleText}>{film.title}</Text>
-                    <Text style={styles.voteText}>{film.vote_average}</Text>
+                        {this._displayFavoriteImage()}
+                        <Text style={styles.titleText}>{film.title}</Text>
+                        <Text style={styles.voteText}>{film.vote_average}</Text>
                     </View>
                     <View style={styles.descriptionContainer}>
                         <Text style={styles.descriptionText} numberOfLines={6}>{film.overview}</Text>
@@ -42,6 +54,11 @@ const styles = StyleSheet.create({
     headerContainer: {
         flex: 3,
         flexDirection: 'row'
+    },
+    favorite_image: {
+        width: 25,
+        height: 25,
+        marginRight: 5
     },
     titleText: {
         fontWeight: 'bold',
