e3d45afb5d00f789ca1f017e9c5fab6b322c5dc9
rodriguezny <rodrigueznyiringabo@gmail.com>
2021-08-11 11:36:05 +0200

    ft - add tab bar icons to the tab navigator

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
@@ -22,10 +24,36 @@ const SearchStackNavigator = createStackNavigator({
 
 const MoviesTabNavigator = createBottomTabNavigator({
     Search: {
-        screen: SearchStackNavigator
+        screen: SearchStackNavigator,
+        navigationOptions: {
+            tabBarIcon: () => {
+                return <Image source={require('../Images/ic_search.png')} style={styles.icon} />
+            }
+        }
     },
     Favorites: {
-        screen: Favorites
+        screen: Favorites,
+        navigationOptions: {
+            tabBarIcon: () => {
+                return <Image source={require('../Images/ic_favorite.png')} style={styles.icon} />
+            }
+        }
+    },
+},
+{
+    tabBarOptions: {
+        activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+        inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+        showLabel: true, // On affiche les titres
+        showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+
+    }
+})
+
+const styles = StyleSheet.create({
+    icon: {
+        height: 30,
+        width: 30
     }
 })
 
